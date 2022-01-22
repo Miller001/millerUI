38c9e9274e828f0bb4fd8c544d678c99426e1a2f
Miller <2473055574@qq.com>
2022-01-22 13:55:59 +0800

    finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,12 @@
 module.exports = {
+	title:'Miller-UI',
 	base: '/millerUI/',
 	themeConfig: {
+		nav:[
+			{text:Home,link:'/'}.
+			{text:'Github',link:'https://github.com/Miller001/millerUI'},
+			{text:'NPM',link:'https://www.npmjs.com/package/miller-ui'}
+		],
 		sidebar: ['/', '/componentDocs/card'],
 	},
 };
